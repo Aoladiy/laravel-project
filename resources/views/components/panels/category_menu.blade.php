089e1edac7a1b5682b93f791a9f5f334810ed9c2
Aldar <ochirov-3@mail.ru>
2023-07-25 23:26:44 +0800

    261954 / Интеграция верстки главной страницы - Задание выполнено

---
+++ resources/views/components/panels/category_menu.blade.php
@@ -0,0 +1,53 @@
+<nav class="order-1">
+    <ul class="block lg:flex">
+        <li class="group">
+            <a class="inline-block p-4 text-black font-bold border-l border-r border-transparent group-hover:text-orange group-hover:bg-gray-100 group-hover:border-l group-hover:border-r group-hover:border-gray-200 group-hover:shadow"
+               href="/catalog">
+                Легковые
+                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-4 w-4" fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M19 9l-7 7-7-7"/>
+                </svg>
+            </a>
+
+            <ul class="dropdown-navigation-submenu absolute hidden group-hover:block bg-white shadow-lg">
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Седаны</a></li>
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Хетчбеки</a></li>
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Универсалы</a></li>
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Купе</a></li>
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Родстеры</a></li>
+            </ul>
+        </li>
+        <li class="group">
+            <a class="inline-block p-4 text-black font-bold border-l border-r border-transparent group-hover:text-orange group-hover:bg-gray-100 group-hover:border-l group-hover:border-r group-hover:border-gray-200 group-hover:shadow"
+               href="/catalog">
+                Внедорожники
+                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-4 w-4" fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M19 9l-7 7-7-7"/>
+                </svg>
+            </a>
+            <ul class="dropdown-navigation-submenu absolute hidden group-hover:block bg-white shadow-lg">
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Рамные</a></li>
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Пикапы</a></li>
+                <li><a class="block py-2 px-4 text-black hover:text-orange hover:bg-gray-100"
+                       href="/catalog">Кроссоверы</a></li>
+            </ul>
+        </li>
+        <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange"
+                             href="/catalog">Раритетные</a></li>
+        <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange"
+                             href="/catalog">Распродажа</a></li>
+        <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange"
+                             href="/catalog">Новинки</a></li>
+    </ul>
+</nav>
